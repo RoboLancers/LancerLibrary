d4ea7c9365455885e863ba4ebe11c231118fbb46
Brian Chen <brianchen0810@hotmail.com>
2018-12-29 23:17:01 -0500

    Add BaseController

---
+++ src/main/java/com/robolancers/lib/wrappers/hid/BaseController.java
@@ -0,0 +1,22 @@
+package com.robolancers.lib.wrappers.hid;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+abstract class BaseController {
+    private static final int NUMBER_OF_BUTTONS = 13;
+    static final double DEADZONE = 0.1;
+
+    Joystick joystick;
+    Button[] buttons;
+
+    BaseController(int port) {
+        joystick = new Joystick(port);
+        buttons = new JoystickButton[NUMBER_OF_BUTTONS];
+
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JoystickButton(joystick, i);
+        }
+    }
+}
