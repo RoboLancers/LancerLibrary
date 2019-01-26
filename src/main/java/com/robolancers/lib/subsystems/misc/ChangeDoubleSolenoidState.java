fb92ed47ca992d97486ddd5bdbaf5888506026ad
Brian Chen <brianchen0810@hotmail.com>
2019-01-25 22:21:37 -0500

    Add ChangeDoubleSolenoidState

---
+++ src/main/java/com/robolancers/lib/subsystems/misc/ChangeDoubleSolenoidState.java
@@ -0,0 +1,22 @@
+package com.robolancers.lib.subsystems.misc;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ChangeDoubleSolenoidState extends InstantCommand {
+    private DoubleSolenoid doubleSolenoid;
+    private DoubleSolenoid.Value value;
+
+    public ChangeDoubleSolenoidState(Subsystem subsystem, DoubleSolenoid doubleSolenoid, DoubleSolenoid.Value value){
+        requires(subsystem);
+
+        this.doubleSolenoid = doubleSolenoid;
+        this.value = value;
+    }
+
+    @Override
+    protected void initialize(){
+        doubleSolenoid.set(value);
+    }
+}
