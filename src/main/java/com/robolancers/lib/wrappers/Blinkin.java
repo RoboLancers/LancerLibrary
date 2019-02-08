f1abfeb66cc6af3a22dca5e952da7ba437ae0d2f
Tomdotexe <nothomasdo@gmail.com>
2019-02-08 15:54:04 -0500

    Added LED Color Patterns

--- src/main/java/com/robolancers/lib/wrappers/Blinkin.java
+++ src/main/java/com/robolancers/lib/wrappers/Blinkin.java
@@ -21,7 +21,15 @@ public class Blinkin {
     public enum PatternType {
         // TODO Finish putting all possible values here
         RAINBOW_RAINBOW_PALETTE(-0.99),
-        RAINBOW_PARTY_PALETTE(-0.97);
+        RAINBOW_PARTY_PALETTE(-0.97),
+        HEARTBEAT_FAST(0.07),
+        STROBE_GOLD(-0.07),
+        STROBE_RED(-0.11),
+        OCEAN_TWINK(-0.51),
+        HEARTBEAT_RED(-0.25),
+        WAVE_PARTY(-0.43),
+        CONFETTI(-0.87),
+        THE_END(-0.03);
 
         double value;
 
