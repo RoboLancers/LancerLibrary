f9e6060b46fbaad84056ab69b7bda96d2d5feabd
Brian Chen <brianchen0810@hotmail.com>
2019-02-08 23:24:34 -0500

    Add LinkedLimitSwitches

---
+++ src/main/java/com/robolancers/lib/wrappers/sensors/LinkedLimitSwitches.java
@@ -0,0 +1,22 @@
+package com.robolancers.lib.wrappers.sensors;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import java.util.stream.Stream;
+
+@SuppressWarnings("unused")
+public class LinkedLimitSwitches {
+    private DigitalInput[] limitSwitches;
+
+    public LinkedLimitSwitches(int... ports){
+        limitSwitches = new DigitalInput[ports.length];
+
+        for(int i = 0; i < limitSwitches.length; i++){
+            limitSwitches[i] = new DigitalInput(ports[i]);
+        }
+    }
+
+    public boolean get(){
+        return Stream.of(limitSwitches).anyMatch(DigitalInput::get);
+    }
+}
